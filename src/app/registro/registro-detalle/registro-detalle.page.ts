2d2d5348cdf9fd872cdc18544945a3b5b13151cc
Josemoct00 <jose.ricaurtem99@outlook.com>
2022-03-15 20:18:38 -0500

    Registro funcionando correctamente

--- src/app/registro/registro-detalle/registro-detalle.page.ts
+++ src/app/registro/registro-detalle/registro-detalle.page.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute} from '@angular/router'; //Me permite mirar los parametros de la URL
+import { RegistroService } from '../registro.service'; //Utilizamos nuevamente el servicio para acceder a metodos de la BD
+import { Viaje } from './registro.model'; //Utilizamos el modelo viaje
 
 @Component({
   selector: 'app-registro-detalle',
@@ -7,9 +10,25 @@ import { Component, OnInit } from '@angular/core';
 })
 export class RegistroDetallePage implements OnInit {
 
-  constructor() { }
+  viaje : Viaje //Propiedad de tipo Viaje
 
-  ngOnInit() {
+  imagen : String
+
+  constructor(private activatedRoute : ActivatedRoute, private registroService : RegistroService) { }
+
+  ngOnInit() { //Inicio al cargar la pagina
+    this.activatedRoute.paramMap.subscribe(paramMap =>{ //ParaMap: Obtener la URL (PARAMETROS) , Suscribe: Recorre todos los parametros
+      //Redireccionamos 
+      const idRecibida = paramMap.get('registroID'); //Guardo en idRecibida el id obtenido
+      this.viaje = this.registroService.getViaje(idRecibida);
+      console.log(this.viaje);
+    })
+    if(this.viaje.servicio == 'Troncal'){
+      this.imagen = 'https://www.transmilenio.gov.co/publicaciones/151309/ya-ruedan-por-la-ciudad-los-primeros-336-buses-nuevos-de-transmilenio/info/transmilenio/media/galeria290630.jpg';
+    }else{
+      this.imagen = 'https://www.kienyke.com/sites/default/files/2021-01/Transmilenio-buses-nuevos-a-gas.jpg'
+    }
+    console.log(this.imagen);
   }
 
 }
--- src/app/registro/registro.page.ts
+++ src/app/registro/registro.page.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import {RegistroService} from './registro.service'
+import {RegistroService} from './registro.service'; //Llamo al servicio que me da conexion con BD
 
 @Component({
   selector: 'app-registro',
@@ -10,9 +10,9 @@ export class RegistroPage implements OnInit {
 
   viajes = []
 
-  constructor(private registroService : RegistroService) { } //RegistroService solo es usado en la clase RegistroPage
+  constructor(private registroService : RegistroService) { } //RegistroService solo sera usado en la clase RegistroPage
 
-  ngOnInit() {
+  ngOnInit() { //Inicio al cargar la pagina
     this.viajes = this.registroService.getViajes()
   }
 
