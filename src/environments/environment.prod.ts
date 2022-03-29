3fde4a9768507d7e76ccb4aae2e8cbd97d7fa2d8
Jose <jose.ricaurte99@gmail.com>
2022-03-28 23:16:33 -0500

    Firebase instalado

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,12 @@
 export const environment = {
-  production: true
+  production: true,
+  firebaseConfig : {
+  apiKey: "AIzaSyBUqV6xp6eyOmB0qrwOqVglMHJw4m5wFzk",
+  authDomain: "fastpassud-7857a.firebaseapp.com",
+  databaseURL: "https://fastpassud-7857a-default-rtdb.firebaseio.com",
+  projectId: "fastpassud-7857a",
+  storageBucket: "fastpassud-7857a.appspot.com",
+  messagingSenderId: "945765345734",
+  appId: "1:945765345734:web:30f870d5652d2c727f2ac7"
+  }
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,16 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  firebaseConfig : {
+  apiKey: "AIzaSyBUqV6xp6eyOmB0qrwOqVglMHJw4m5wFzk",
+  authDomain: "fastpassud-7857a.firebaseapp.com",
+  databaseURL: "https://fastpassud-7857a-default-rtdb.firebaseio.com",
+  projectId: "fastpassud-7857a",
+  storageBucket: "fastpassud-7857a.appspot.com",
+  messagingSenderId: "945765345734",
+  appId: "1:945765345734:web:30f870d5652d2c727f2ac7"
+  }
 };
 
 /*
